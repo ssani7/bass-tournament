0902a399158e71be3f6e2259572aceaae55c8d9b
junel <sacrojunel@gmail.com>
2023-04-01 20:44:27 +0800

    feat: add the community menu items

--- components/molecules/header/desktop-navigation/menuitem-community.tsx
+++ components/molecules/header/desktop-navigation/menuitem-community.tsx
@@ -1,3 +1,59 @@
+import {
+   BLOG_ICON,
+   FORUM_ICON,
+   NEWS_ICON,
+   SUPPORT_ICON,
+} from '@/utils/constants/community_image_menuitem';
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const community_menuitems = [
+   {
+      name: 'Forum',
+      icon: FORUM_ICON,
+   },
+   {
+      name: 'News',
+      icon: NEWS_ICON,
+   },
+   {
+      name: 'Blog',
+      icon: BLOG_ICON,
+   },
+   {
+      name: 'Support',
+      icon: SUPPORT_ICON,
+   },
+];
+
 export const MenuItemCommunity = () => {
-   return <div>MenuItemCommunity</div>;
+   return (
+      <div className="inline-flex mt-16 ml-6 group">
+         {community_menuitems.map(({ name, icon }) => (
+            <Link
+               key={name}
+               href={'/'}
+               className={clsx(
+                  'block relative mx-6 w-20',
+                  'text-[rgb(210,210,210)] transition-[opacity,color]',
+                  'ease-[ease,ease] duration-[0.3s,0.3s] delay-[0s,0s]',
+                  'group-hover:text-[#fcfcfc]',
+                  'group-hover:opacity-[0.6] hover:!opacity-100',
+               )}
+            >
+               <Image
+                  className="hover:!scale-[1.1] hover:translate-y-[-4px] transition-[transform] duration-[0.3s] ease-[ease] delay-[0s] translate-y-0"
+                  src={icon}
+                  alt="forum"
+                  width={80}
+                  height={80}
+               />
+               <span className="text-lg block text-center font-bold mt-3">
+                  {name}
+               </span>
+            </Link>
+         ))}
+      </div>
+   );
 };
---
+++ utils/constants/community_image_menuitem/index.ts
@@ -0,0 +1,13 @@
+const FORUM_ICON =
+   'https://cdn01.x-plarium.com/browser/content/portal/common/icons/v3/new/forum.svg';
+
+const NEWS_ICON =
+   'https://cdn01.x-plarium.com/browser/content/portal/common/icons/v3/new/news.svg';
+
+const BLOG_ICON =
+   'https://cdn01.x-plarium.com/browser/content/portal/common/icons/v3/new/blog.svg';
+
+const SUPPORT_ICON =
+   'https://cdn01.x-plarium.com/browser/content/portal/common/icons/v3/new/support.svg';
+
+export { FORUM_ICON, NEWS_ICON, BLOG_ICON, SUPPORT_ICON };
