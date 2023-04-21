3f3f2294e3abdade46987251804e00632349c43c
junel <sacrojunel@gmail.com>
2023-04-21 17:16:43 +0800

    feat: commits

--- components/organism/crossplatform/index.tsx
+++ components/organism/crossplatform/index.tsx
@@ -4,44 +4,45 @@ import { GAMES } from '@/utils/constants';
 import Image from 'next/image';
 
 export const CrossPlatformSection = () => {
-    return (
-        <div className="pt-[40px] pb-[80px] bg-[#191b1f] flex justify-center w-full ">
-            <Card
-                variant={'dark'}
-                containerClassName={
-                    'rounded-[8px] pr-[55px] pb-[45px] pt-[35px] pl-[12px]'
-                }
-            >
-                <div className="flex w-[1140px]">
-                    <div className="relative lg: w-3/5 flex flex-shrink-0 justify-center">
-                        <Image
-                            className="object-contain"
-                            placeholder={'blur'}
-                            blurDataURL={GAMES}
-                            src={GAMES}
-                            alt="games"
-                            fill
-                        />
-                    </div>
-                    <div className="flex flex-col ml-10 pt-16">
-                        <div className="text-white font-extrabold text-4xl mb-4">
-                            CROSS-PLATFORM GAMES
-                        </div>
-                        <div className="text-white/80 mb-8">
-                            Get Plarium Play for PC or Mac to play our mobile
-                            games for free anytime, everywhere!
-                        </div>
-                        <div>
-                            <Button
-                                variant={ButtonVariant.PRIMARY}
-                                size={ButtonSize.EXTRA_LARGE}
-                            >
-                                Get Plarium Play
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-            </Card>
-        </div>
-    );
+   return (
+      <div className="pt-[40px] pb-[80px] bg-[#191b1f] flex justify-center w-full">
+         <Card
+            variant={'dark'}
+            containerClassName={
+               'rounded-[8px] pr-[55px] pb-[45px] pt-[35px] pl-[12px]'
+            }
+         >
+            <div className="flex flex-col-reverse lg:flex-row md:w-full lg:w-[1140px]">
+               <div className="relative md:mx-8 md:h-[360px] md:w-[585px] lg: w-3/5 flex flex-shrink-0 justify-center items-center">
+                  <Image
+                     className="object-contain"
+                     placeholder={'blur'}
+                     blurDataURL={GAMES}
+                     src={GAMES}
+                     alt="games"
+                     fill
+                  />
+               </div>
+               <div className="flex flex-col ml-10 pt-16">
+                  <div className="text-white font-extrabold text-4xl mb-4">
+                     CROSS-PLATFORM GAMES
+                  </div>
+                  <div className="text-white/80 mb-8">
+                     Get Plarium Play for PC or Mac to play our mobile games for
+                     free anytime, everywhere!
+                  </div>
+                  <div>
+                     <Button
+                        containerClassName="sm:max-md:invisible"
+                        variant={ButtonVariant.PRIMARY}
+                        size={ButtonSize.EXTRA_LARGE}
+                     >
+                        Get Plarium Play
+                     </Button>
+                  </div>
+               </div>
+            </div>
+         </Card>
+      </div>
+   );
 };
