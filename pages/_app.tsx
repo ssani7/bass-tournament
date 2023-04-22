d2631280d60db49523a52b616563b999bbced407
junel <sacrojunel@gmail.com>
2023-04-22 20:29:56 +0800

    fix: fix  the font that not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
-import "@/styles/globals.css";
-import "swiper/css";
-import "swiper/css/autoplay";
-import type { AppProps } from "next/app";
-import { Inter, Anton } from "next/font/google";
+import '@/styles/globals.css';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import type { AppProps } from 'next/app';
+import { Inter, Anton } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const anton = Anton({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-anton",
+   weight: '400',
+   subsets: ['latin'],
+   variable: '--font-anton',
 });
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={`${inter.variable} font-sans ${anton.variable} `}>
-      <Component {...pageProps} />
-    </main>
-  );
+   return (
+      <main className={`${inter.variable} font-sans ${anton.variable}`}>
+         <Component {...pageProps} />
+      </main>
+   );
 }
